fdb856a78c3adb2ce20daf55a58fe76023fdb5bc
Sandino <sandinoandroid@gmail.com>
2019-03-12 22:42:02 -0300

    Feature: start text robot  Changes to be committed: 	modified:   .gitignore 	new file:   credentials/README.md 	modified:   index.js 	modified:   package.json 	modified:   robots/text.js 	modified:   yarn.lock

---
+++ credentials/README.md
@@ -0,0 +1,11 @@
+# Credentials Format
+
+## Algorithmia
+
+File: `algorithmia.json`
+
+```
+{
+    "apiKey":"simx2RioJHYUypR9zu4dma/32w01"
+}
+```
--- robots/text.js
+++ robots/text.js
@@ -1,4 +1,60 @@
-function robot(content){
-    console.log(`Recebi com sucesso o content: ${content.search}`);
+const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey;
+const algorithmia = require('algorithmia');
+const sentenceBoundaryDetection = require('sbd');
+
+async function robot(content){
+    await fetchContentFromWikipedia(content)
+    sanitizeContent(content)
+    breakContentIntoSentences(content)
+
+    async function fetchContentFromWikipedia(content){
+        const algorithmiaAuthenticated = algorithmia('simx2RioJHYUypR9zu4dma/32w01')
+        const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+        const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
+        const wikipediaContent = wikipediaResponse.get()
+
+        content.sourceContentOriginal = wikipediaContent.content
+    }
+
+    function sanitizeContent(content) {
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+        const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+
+        content.sourceContentSanitized = withoutDatesInParentheses
+
+        //Função que remove linhas em branco
+        function removeBlankLinesAndMarkdown(text){
+            const allLines = text.split('\n')
+
+            const withoutBlankLinesAndMarkdown = allLines.filter(line => {
+                if(line.trim().length === 0 || line.trim().startsWith('=')){
+                    return false
+                }
+                return true
+            })
+            return withoutBlankLinesAndMarkdown.join(' ')
+        }
+        //Função que remove datas entre parênteses
+        function removeDatesInParentheses(text){
+            return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+        }
+        //Função que separa o texto em sentenças.
+    }
+
+    //Função que quebra o texto em sentenças
+    function breakContentIntoSentences(content){
+        content.sentences = []
+
+        const sentences =  sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+        sentences.forEach(sentence => {
+            content.sentences.push({
+                text: sentence,
+                keywords: [],
+                images: []
+            })
+        })
+        //console.log(sentences);
+
+    }
 }
 module.exports = robot;
